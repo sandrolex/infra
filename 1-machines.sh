84dff5213de6d90d31d5d7c2a78a639df6d00c1b
sandro-lex-symphony <sandro-lex@symphony.com>
2021-02-16 13:20:21 +0100

    using user-data

--- 1-machines.sh
+++ 1-machines.sh
@@ -2,7 +2,7 @@
 
 create_vm() {
     echo "creating $1"
-    id=$(microstack.openstack server create --image Bionic --flavor m1.large --security-group aa715ebf-6e06-4540-b6c5-7bdb50571e1b --security-group Open --nic net-id=test --key-name microstack $1 --format json | jq -r .id)
+    id=$(microstack.openstack server create --image Bionic --flavor m1.large --security-group aa715ebf-6e06-4540-b6c5-7bdb50571e1b --security-group Open --nic net-id=test --key-name microstack --user-data 2-prepare-vm.sh $1 --format json | jq -r .id)
     echo "created machine $id"
 
     ip=$(microstack.openstack floating ip create -f value -c floating_ip_address 'external')
