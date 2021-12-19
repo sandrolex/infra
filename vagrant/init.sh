3e0ad40e13ff94426c577e5271f3612f1bf34287
Sandro Lex <sandro.lex@symphony.com>
2021-12-19 11:03:39 +0100

    create local infra with vagrant

---
+++ vagrant/init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+
+apt-get update -y
+apt-get install docker.io -y
+systemctl enable docker
+systemctl start docker
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+apt-get install kubeadm kubelet kubectl -y
+apt-mark hold kubeadm kubelet kubectl
+swapoff -a
+ufw disable
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+systemctl daemon-reload
+systemctl restart docker
+
+
+iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
+
+systemctl start kubelet
+
