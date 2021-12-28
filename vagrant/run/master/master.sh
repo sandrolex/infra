2fc5f32b68af1ba4f65692bf5f0029f9f628c201
Sandro Lex <sandro.lex@symphony.com>
2021-12-28 11:54:35 +0100

    up

--- vagrant/run/master/master.sh
+++ vagrant/run/master/master.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-
-sed -i 's/xxx/192.168.1.192/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+echo "prepare kubelet"
+sed -i '5 i Environment="KUBELET_EXTRA_ARGS=--node-ip=xxx"' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 systemctl daemon-reload
 systemctl restart kubelet
+sleep 10
 
+echo "kubeadm"
 kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.1.192
 
 
