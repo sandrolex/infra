8dfa3555951f7fbd2955046758c868526c0f0847
Sandro Lex <sandro.lex@symphony.com>
2021-12-28 11:17:20 +0100

    up

--- vagrant/init.sh
+++ vagrant/init.sh
@@ -31,3 +31,21 @@ iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
 
 systemctl start kubelet
 
+cat > /etc/systemd/system/kubelet.service.d/10-kubeadm.conf <<EOF
+# with kubeadm and kubelet v1.11+
+[Service]
+Environment="KUBELET_KUBECONFIG_ARGS=--bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf --kubeconfig=/etc/kubernetes/kubelet.conf"
+Environment="KUBELET_CONFIG_ARGS=--config=/var/lib/kubelet/config.yaml"
+Environment="KUBELET_EXTRA_ARGS=--node-ip=xxx"
+# This is a file that "kubeadm init" and "kubeadm join" generates at runtime, populating the KUBELET_KUBEADM_ARGS variable dynamically
+EnvironmentFile=-/var/lib/kubelet/kubeadm-flags.env
+# This is a file that the user can use for overrides of the kubelet args as a last resort. Preferably, the user should use
+# the .NodeRegistration.KubeletExtraArgs object in the configuration files instead. KUBELET_EXTRA_ARGS should be sourced from this file.
+EnvironmentFile=-/etc/default/kubelet
+ExecStart=
+ExecStart=/usr/bin/kubelet $KUBELET_KUBECONFIG_ARGS $KUBELET_CONFIG_ARGS $KUBELET_KUBEADM_ARGS $KUBELET_EXTRA_ARGS
+EOF
+
+systemctl daemon-reload
+systemctl restart kubelet
+
---
+++ vagrant/run/master/master.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+sed -i 's/xxx/192.168.1.192/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
+kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.1.192
+
+
+
