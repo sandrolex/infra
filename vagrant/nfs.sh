c5ba7bfc720d20ba939feb56b20ab925e1dbdc51
Sandro Lex <sandro.lex@symphony.com>
2021-12-28 18:54:03 +0100

    up

--- vagrant/nfs.sh
+++ vagrant/nfs.sh
@@ -4,12 +4,16 @@ apt-get update -y
 apt-get install nfs-kernel-server -y 
 
 mkdir -p /mnt/nfs_share
+mkdir -p /mnt/kube_logs
 
 chown -R nobody:nogroup /mnt/nfs_share/
+chown -R nobody:nogroup /mnt/kube_logs/
 
 chmod 777 /mnt/nfs_share/
+chmod 777 /mnt/kube_logs/
 
-echo "/mnt/nfs_share  192.168.1.0/24(rw,sync,no_subtree_check,no_root_squash)" > /etc/exports
+echo "/mnt/nfs_share  192.168.1.0/24(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
+echo "/mnt/kube_logs  192.168.1.0/24(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
 
 exportfs -a
 systemctl restart nfs-kernel-server
--- vagrant/run/master/master.sh
+++ vagrant/run/master/master.sh
@@ -10,4 +10,7 @@ echo "kubeadm"
 kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.1.192
 
 
+mkdir -p /home/vagrant/.kube
+sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+sudo chown 1000:1000 /home/vagrant/.kube/config
 
