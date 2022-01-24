9e2c15bcc5227b38005ed75b462d6865406cdd86
Sandro Lex <sandrolex@gmail.com>
2022-01-24 10:57:56 +0100

    add worker3

---
+++ vagrant/run/worker3/worker3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "prepare kubelet"
+sed -i '5 i Environment="KUBELET_EXTRA_ARGS=--node-ip=192.168.1.198"' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+systemctl daemon-reload
+systemctl restart kubelet
