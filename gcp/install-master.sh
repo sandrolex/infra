f0382dc4d287389c7902296559913d899e6ca05e
sandrolex <sandrolex@gmail.com>
2022-05-23 15:22:49 +0200

    Create install-master.sh

---
+++ gcp/install-master.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+apt-get update -y
+
+apt -y install curl apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+
+apt update
+apt -y install vim git curl wget kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+swapoff -a
+
+modprobe overlay
+modprobe br_netfilter
+
+# Add some settings to sysctl
+tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+# Reload sysctl
+sysctl --system
+
+# Configure persistent loading of modules
+tee /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+# Load at runtime
+modprobe overlay
+modprobe br_netfilter
+
+# Ensure sysctl params are set
+tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+# Reload configs
+sysctl --system
+
+# Install required packages
+apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+
+# Add Docker repo
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Install containerd
+apt update
+apt install -y containerd.io
+
+# Configure containerd and start service
+mkdir -p /etc/containerd
+containerd config default>/etc/containerd/config.toml
+
+# restart containerd
+systemctl restart containerd
+systemctl enable containerd
+#systemctl status  containerd
+
+systemctl enable kubelet
+
+kubeadm init
+
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
